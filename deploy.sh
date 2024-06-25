9944257a29e33df26229897fdc6d946f35162853
Hari <118122494+Hari0o@users.noreply.github.com>
2024-06-25 16:17:16 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pods_id=$(microk8s kubectl get pods -n react-nginx |grep react |awk {'print $1'})
-microk8s kubectl delete pod $pods_id -n react-nginx 
+cd react-chart/
+git pull
+helm upgrade react-chart-helm .
