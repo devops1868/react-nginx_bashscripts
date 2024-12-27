f5397bf0b3d08af6d8cc893b81538b224b27082d
devops1868 <151348275+devops1868@users.noreply.github.com>
2024-12-27 13:53:38 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
-
 #!/bin/bash
-sudo docker rmi -f $(sudo docker images -q) ##this is not recommned step, i am deleting existing images to save space
+#sudo docker rmi -f $(sudo docker images -q) ##this is not recommned step, i am deleting existing images to save space
 sudo rm -r gold ## these steps are not recommened instead you can modify script as shown below
 sudo mkdir gold
 cd gold/
-sudo git clone https://github.com/Hari0o/Gold_Site_Ecommerce.git
+sudo git clone https://github.com/devops1868/Gold_Site_Ecommerce.git
 cd Gold_Site_Ecommerce/
-sudo docker build -t react-nginx -f golddockerfile .
-sudo docker tag react-nginx:latest sagarkakkala385/react-nginx:latest ##make sure you did docker login
-sudo docker push sagarkakkala385/react-nginx:latest
+sudo docker build -t devops1868/react-nginx:latest -f DockerFile .
+#sudo docker tag react-nginx:latest sagarkakkala385/react-nginx:latest ##make sure you did docker login
+sudo docker push devops1868/react-nginx:latest
 
 
 ##recommended script###
